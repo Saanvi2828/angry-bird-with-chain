67741bb45f1633546d47b607c45966b82d74ff8d
Your Name <manishgupta_tcsdelhi@yahoo.com>
2020-06-29 17:59:31 +0530

    anry chain

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,9 +35,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(100,100,100,PI/4);
 
     bird = new Bird(100,100);
 
+    chain=new Chain(bird.body,log6.body);
 }
 
 function draw(){
@@ -61,4 +65,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    chain.display();
+    log6.display();
 }
